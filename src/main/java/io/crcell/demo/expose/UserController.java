b7f8259d0c21ae4f50928298b455118d18f3c6ac
PotatoWhite <bravopotato@gmail.com>
2021-01-12 02:02:54 +0900

    add custom controller

--- src/main/java/io/crcell/demo/expose/UserController.java
+++ src/main/java/io/crcell/demo/expose/UserController.java
@@ -4,6 +4,9 @@ import io.crcell.demo.entities.User;
 import io.crcell.demo.logic.UserService;
 import io.crcell.pramework.controllable.ControllableImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController extends ControllableImpl<User, Long> {
+  private final UserService userService;
+
   public UserController(UserService userService) {
     super(userService);
+    this.userService = userService;
+  }
+
+  @GetMapping
+  private ResponseEntity findAll() {
+    return ResponseEntity.status(HttpStatus.OK)
+                         .body(userService.findAll());
   }
 }
--- src/main/java/io/crcell/demo/logic/UserService.java
+++ src/main/java/io/crcell/demo/logic/UserService.java
@@ -6,10 +6,20 @@ import io.crcell.pramework.serviceable.ServiceableImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
 @Service
 public class UserService extends ServiceableImpl<User, Long> {
+  private final UserRepository userRepository;
+
   public UserService(UserRepository repository) {
     super(repository);
+    this.userRepository = repository;
+  }
+
+
+  public List<User> findAll() {
+    return userRepository.findAll();
   }
 }
